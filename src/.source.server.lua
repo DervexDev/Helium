a5af1647bc3a6ea585881d67ba77559b2e0f18af
DARK <david92005999@gmail.com>
2023-01-24 22:53:12 +0100

    Fixed shared toolbar

--- src/.source.server.lua
+++ src/.source.server.lua
@@ -1,38 +1,16 @@
-local CoreGui = game:GetService('CoreGui')
-
 local Gui = require(script.Gui)
 local Keybinds = require(script.Keybinds)
 local Config = require(script.Config)
-
-local toolbar
-
-if CoreGui:FindFirstChild("Dervex' utils") and #CoreGui["Dervex' utils"]:GetChildren() > 0 then
-    toolbar = CoreGui["Dervex' utils"].Value
-    print(1)
-else
-    print(CoreGui["Dervex' utils"]:GetChildren())
-    if CoreGui:FindFirstChild("Dervex' utils") then
-        CoreGui["Dervex' utils"]:Destroy()
-    end
-
-    toolbar = plugin:CreateToolbar("Dervex' utils")
-
-    local toolbarRef = Instance.new('ObjectValue', CoreGui)
-    toolbarRef.Name = "Dervex' utils"
-    toolbarRef.Value = toolbar
-end
-
-local button = toolbar:CreateButton('Helium', 'Show Helium UI', 'rbxassetid://12243614008')
+local Toolbar = require(script.Toolbar)
 
 local widgetInfo = DockWidgetPluginGuiInfo.new(Enum.InitialDockState.Float, false, false, 300, 250, 300, 130)
 local widget = plugin:CreateDockWidgetPluginGui('Helium', widgetInfo)
+local button = Toolbar('Helium', plugin)
 
 widget.Name = 'Helium'
 widget.Title = 'Helium'
 widget.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 script.Parent.HeliumGui.Root.Background.Parent = widget
-button.ClickableWhenViewportHidden = true
-Instance.new('ObjectValue', CoreGui["Dervex' utils"]).Name = 'Helium'
 
 Config.init(plugin)
 
@@ -72,10 +50,3 @@ end
 if Config.keybindsEnabled then
     Keybinds.bind()
 end
-
-plugin.Unloading:Connect(function()
-    if CoreGui:FindFirstChild("Dervex' utils") and CoreGui["Dervex' utils"]:FindFirstChild('Helium') then
-        CoreGui["Dervex' utils"].Helium:Destroy()
-        button:Destroy()
-    end
-end)
